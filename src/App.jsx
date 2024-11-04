0077c200ffbb39f3c6291b3b034ded25ea215277
Argxz <riyanargaw@gmail.com>
2024-11-04 23:06:54 +0700

    init config files

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import List from './pages/List';
+import About from './pages/About';
+import Detail from './pages/Detail';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/list-anime" element={<List />} />
+        <Route path="/detail/:animeId" element={<Detail />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
