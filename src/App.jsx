02c6297796ea0dc1962f8df0596a1b97f23c1bf9
Argxz <riyanargaw@gmail.com>
2024-11-04 23:10:47 +0700

    init structure folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-import List from './pages/List';
-import About from './pages/About';
 import Detail from './pages/Detail';
 
 
@@ -11,9 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/list-anime" element={<List />} />
         <Route path="/detail/:animeId" element={<Detail />} />
-        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
