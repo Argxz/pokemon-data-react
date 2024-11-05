9b2e9a952052f1e4ef239aafb9142d3583789d8f
Argxz <riyanargaw@gmail.com>
2024-11-05 18:42:43 +0700

    dashboard styling

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,28 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPokemons } from '../redux/action';
 import { useNavigate } from 'react-router-dom';
+import '../styles/Dashboard.css'
+
+const typeColors = {
+    normal: '#A8A77A',
+    fire: '#EE8130',
+    water: '#6390F0',
+    grass: '#7AC74C',
+    electric: '#F7D02C',
+    ice: '#96D9D6',
+    fighting: '#C22E28',
+    poison: '#A33EA1',
+    ground: '#E2BF65',
+    flying: '#A98FF3',
+    psychic: '#F95587',
+    bug: '#A6B91A',
+    rock: '#B6A136',
+    ghost: '#735797',
+    dragon: '#6F35FC',
+    dark: '#705746',
+    fairy: '#D685AD',
+    steel: '#B7B7CE',
+};
 
 const PokemonList = () => {
     const dispatch = useDispatch();
@@ -27,22 +49,30 @@ const PokemonList = () => {
     if (error) return <p>Error: {error}</p>;
 
     return (
-        <div className="pokemon-list" style={{ display: 'grid', gridTemplateColumns: 'repeat(6, 1fr)', gap: '10px' }}>
-            {pokemons.map((pokemon, index) => (
-                <div key={index} className="pokemon-card" onClick={() => handleDetailClick(pokemon)} style={{ padding: '10px', border: '1px solid #ddd', cursor: 'pointer', textAlign: 'center' }}>
-                    <img src={pokemon.sprites.front_default} alt={pokemon.name} style={{ width: '80px', height: '80px' }} />
-                    <p style={{ fontWeight: 'bold', margin: '5px 0' }}>{pokemon.name}</p>
-                    <div>
-                        {pokemon.types.map((type, idx) => (
-                            <span key={idx} style={{ margin: '0 5px', padding: '2px 5px', backgroundColor: '#eee', borderRadius: '4px' }}>
-                                {type.type.name}
-                            </span>
-                        ))}
+        <div className="pokemon-list-container">
+            <h2 className="pokedex-title">Pokedex Data</h2>
+            <div className="pokemon-list">
+                {pokemons.map((pokemon, index) => (
+                    <div key={index} className="pokemon-card" onClick={() => handleDetailClick(pokemon)}>
+                        <img src={pokemon.sprites.front_default} alt={pokemon.name} className="pokemon-image" />
+                        <p className="pokemon-name">{pokemon.name}</p>
+                        <div className="pokemon-types">
+                            {pokemon.types.map((type, idx) => (
+                                <span 
+                                    key={idx} 
+                                    className="pokemon-type" 
+                                    style={{ backgroundColor: typeColors[type.type.name], color: '#fff' }}
+                                >
+                                    {type.type.name}
+                                </span>
+                            ))}
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
+    
 };
 
 export default PokemonList;
